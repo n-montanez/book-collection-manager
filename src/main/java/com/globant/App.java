9fe4a8abe5cf4f65626b220301d48d7469e44083
Nicolás Montañez Velasco <nicolas.montanez@globant.com>
2025-03-25 08:46:30 -0500

    feat: adding extra options to menu

--- src/main/java/com/globant/App.java
+++ src/main/java/com/globant/App.java
@@ -1,8 +1,6 @@
 package com.globant;
 
 import com.globant.config.DataInitializer;
-import com.globant.model.base.Author;
-import com.globant.model.base.Book;
 import com.globant.model.dao.AuthorDAO;
 import com.globant.model.dao.BookDAO;
 import jakarta.persistence.EntityManager;
@@ -12,6 +10,8 @@ import jakarta.persistence.PersistenceException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Scanner;
+
+import static com.globant.BookStoreManager.*;
 
 @Slf4j
 public class App {
@@ -32,21 +32,33 @@ public class App {
             do {
                 printMenu();
                 int option = scanner.nextInt();
+                scanner.nextLine();
 
                 switch (option) {
                     case 1:
-                        System.out.println("All available books: ");
-                        for (Book book : bookDAO.findAll()) {
-                            System.out.println(book.toString());
-                        }
+                        printBookList(bookDAO);
                         break;
                     case 2:
-                        System.out.println("All available authors: ");
-                        for (Author author : authorDAO.findAll()) {
-                            System.out.println(author.toString());
-                        }
+                        printAuthorList(authorDAO);
                         break;
                     case 3:
+                        printBookList(bookDAO);
+                        System.out.print("Enter book id: ");
+                        long id = scanner.nextLong();
+                        printBookDetails(bookDAO, id);
+                        break;
+                    case 4:
+                        System.out.print("Enter author name: ");
+                        String authorName = scanner.nextLine();
+                        booksByAuthor(bookDAO, authorName);
+                        break;
+                    case 5:
+                        printAvailableGenres();
+                        System.out.print("Enter genre: ");
+                        String genre = scanner.nextLine();
+                        booksByGenre(bookDAO, genre);
+                        break;
+                    case 6:
                         em.close();
                         emf.close();
                         scanner.close();
@@ -61,7 +73,10 @@ public class App {
     private static void printMenu() {
         System.out.println("1. List books");
         System.out.println("2. List authors");
-        System.out.println("3. Exit");
+        System.out.println("3. Show book details");
+        System.out.println("4. Find books by author");
+        System.out.println("5. Find books by genre");
+        System.out.println("6. Exit");
         System.out.print("Enter option: ");
     }
 }
---
+++ src/main/java/com/globant/BookStoreManager.java
@@ -0,0 +1,58 @@
+package com.globant;
+
+import com.globant.model.base.Author;
+import com.globant.model.base.Book;
+import com.globant.model.base.Genre;
+import com.globant.model.dao.AuthorDAO;
+import com.globant.model.dao.BookDAO;
+
+public class BookStoreManager {
+    public static void printAuthorList(AuthorDAO authorDAO) {
+        System.out.println("------------------------------------");
+        System.out.println("All available authors: ");
+        for (Author author : authorDAO.findAll()) {
+            System.out.println(author.getAuthorId() + " - " + author.getName());
+        }
+        System.out.println("------------------------------------");
+    }
+
+    public static void printBookList(BookDAO bookDAO) {
+        System.out.println("------------------------------------");
+        System.out.println("All available books: ");
+        for (Book book : bookDAO.findAll()) {
+            System.out.println(book.getId() + " - " + book.getTitle());
+        }
+        System.out.println("------------------------------------");
+    }
+
+    public static void printBookDetails(BookDAO bookDAO, long id) {
+        Book book = bookDAO.findById(id);
+        if (book == null) {
+            System.out.println("Book " + id + " was not found");
+            return;
+        }
+        System.out.println("------------------------------------");
+        System.out.println(book.getTitle());
+        System.out.println("Written by: " + book.getAuthor().getName());
+        System.out.println("Release year: " + book.getYear());
+        System.out.println("Genre: " + book.getGenre());
+        System.out.println("------------------------------------");
+    }
+
+    public static void printAvailableGenres() {
+        System.out.println("------------------------------------");
+        System.out.println("Available genres: ");
+        for (Genre genre : Genre.values()) {
+            System.out.println(genre.toString());
+        }
+        System.out.println("------------------------------------");
+    }
+
+    public static void booksByAuthor(BookDAO bookDAO, String authorName) {
+        // TODO: Add custom query
+    }
+
+    public static void booksByGenre(BookDAO bookDAO, String genre) {
+        // TODO: Add custom query
+    }
+}
